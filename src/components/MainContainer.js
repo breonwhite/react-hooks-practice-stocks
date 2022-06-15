55caae724e4d9052027c3ec4167457d33a7d5c96
Breon White <bdwhite92@gmail.com>
2022-06-15 17:58:33 -0400

    commit tree 4adadc14364b903caa921c8a1416db95f635cdbd

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then((r) => r.json())
+      .then(setStocks);
+  }, []);
+
+  function handleAddStock(clickedStock) {
+    const stockInPortfolio = portfolio.find(
+      (stock) => stock.id === clickedStock.id
+    );
+    if (!stockInPortfolio) {
+      setPortfolio([...portfolio, clickedStock]);
+    }
+  }
+
+  function handleRemoveStock(stockToRemove) {
+    setPortfolio((portfolio) =>
+      portfolio.filter((stock) => stock.id !== stockToRemove.id)
+    );
+  }
+  
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks} addStock={handleAddStock} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer
+            stocks={portfolio}
+            removeStock={handleRemoveStock}
+          />
         </div>
       </div>
     </div>
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({ stocks, addStock }) {
+  
+  const stocksList  = stocks.map((stock) => (
+    <Stock key={stock.id} stock={stock} onStockClick={addStock} />
+  ));
+  
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stocksList}
     </div>
   );
 }
